28172c723401869173be89b6bbdfd2a6fdd395f3
Brycestampfl <brycestampfl@hotmail.com>
2021-03-01 23:17:14 -0800

    added selecting all and by id to the products reducers

--- react-ui/src/_reducers/productsSlice.js
+++ react-ui/src/_reducers/productsSlice.js
@@ -2,10 +2,10 @@ import { createSlice } from '@reduxjs/toolkit'
 
 
 const initialProductsState =  [  
-    {name: '1', price: 10, description: 'testDescrip1' },
-    {name: '2', price: 20, description: 'testDescrip2' },
-    {name: '3', price: 30, description: 'testDescrip3' },
-    {name: '4', price: 40, description: 'testDescrip4' }]
+    {id: 0, name: '1', price: 10, description: 'testDescrip1' },
+    {id: 1, name: '2', price: 20, description: 'testDescrip2' },
+    {id: 2, name: '3', price: 30, description: 'testDescrip3' },
+    {id: 3, name: '4', price: 40, description: 'testDescrip4' }]
  
 
 export const productsSlice = createSlice({
@@ -15,6 +15,9 @@ export const productsSlice = createSlice({
         addProduct: (state, action) => {
             state.push(action.payload)
         },
+        selectProductById: (state, action) => {
+            state.products.find(product => product.id === action.payload)
+        }
     }
 })
 
