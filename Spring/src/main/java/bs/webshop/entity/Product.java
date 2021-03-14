d0ec4f4298cf182f07f2ad33cf464c2e7c3c0a6c
Brycestampfl <brycestampfl@hotmail.com>
2021-03-13 17:35:46 -0800

    Created product entity and rest controller

---
+++ Spring/src/main/java/bs/webshop/entity/Product.java
@@ -0,0 +1,62 @@
+package bs.webshop.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private double price;
+    private String description;
+  //  private String[] imageUrl;
+
+    public Product(){
+
+    }
+
+
+    public Product(long id, String title, double price, String description) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return title;
+    }
+
+    public void setName(String name) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
