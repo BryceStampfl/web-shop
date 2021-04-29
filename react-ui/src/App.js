df6c13e189c3eb9c9c990b7eb7dc98542f79e952
Brycestampfl <brycestampfl@hotmail.com>
2021-04-28 17:07:01 -0700

    adjusted App file to display routes component and to clear local storage where im currently keeping my jwt token when the app gets reloaded

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,71 +1,23 @@
-import { Container, Row, Col } from 'react-bootstrap'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
 
 
-import Header from "./components/header/Header"
-import store from './store';
-import jwt_decode from "jwt-decode";
-import { setCurrentUser, logout } from './_reducers/usersSlice'
-import Landing from './components/landing/Landing';
-import SecuredRoute from './_securityUtils/SecuredRoute';
-import Dashboard from './components/dashboard/Dashboard';
+import ROUTES from "./_utils/routes"
+import Header from './components/header/Header'
+import RenderRoutes from "./components/RenderRoutes";
 
-import setJwtToken from "./_securityUtils/setJwtToken"
-import Login from './components/landing/Login';
-import Register from './components/landing/Register';
 
-const jwtToken = localStorage.jwtToken
-
-if (jwtToken) {
-  setJwtToken(jwtToken)
-  const decoded_jwtToken = jwt_decode(jwtToken);
-  store.dispatch(setCurrentUser(decoded_jwtToken));
-
-  const currentTime = Date.now() / 1000;
-  if (decoded_jwtToken.exp < currentTime) {
-    store.dispatch(logout());
-    window.location.href = "/";
-  }
+//Clears the local storage if app if refreshed
+window.onbeforeunload = function() {
+  localStorage.clear();
 }
-
 
 function App() {
   return (
     <div className="App">
-      <Router>
-
-        <Header />
-
-        <Container fluid>
-
-          {
-            //public routes
-          }
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/" component={Login} />
-
-
-          {
-            //private routes
-          }
-          <Switch>
-            <SecuredRoute exact path="/dashboard" component={Dashboard} />
-          </Switch>
-          {/* <Row>
-            <Col md={{ span: 6, offset: 3 }}>
-              <Register />
-            </Col>
-          </Row>
-
-          <Row>
-            <Col>
-              <AddProduct />
-              <ProductList />
-            </Col>
-          </Row> */}
-        </Container>
-      </Router>
+      <Header />
+      <RenderRoutes routes={ROUTES} />
     </div>
+
   );
 }
 
