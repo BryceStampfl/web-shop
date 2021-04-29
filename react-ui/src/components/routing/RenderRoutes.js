ebe20e35663b826f037d338756dd822b85270895
Brycestampfl <brycestampfl@hotmail.com>
2021-04-29 10:48:49 -0700

    cleaned up logging

--- react-ui/src/components/routing/RenderRoutes.js
+++ react-ui/src/components/routing/RenderRoutes.js
@@ -6,8 +6,6 @@ const RenderRoutes = ({ routes }) => {
     return (
         <Switch>
             {routes.map((route, i) => {
-                console.log(`Routes are ${route.path}`)
-                
                 return <RouteWithSubRoutes key={route.key} {...route} />;
             })}
             <Route component={() => <h1>Not Found!</h1>} />
@@ -24,7 +22,6 @@ function RouteWithSubRoutes(route) {
             path={route.path}
             exact={route.exact}
             component={props => <route.component routes={route.routes} />}
-
         />
     );
 }
