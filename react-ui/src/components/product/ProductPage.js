eeb306fd0efbcda7d8433aa8e467c706e2d3eba6
Brycestampfl <brycestampfl@hotmail.com>
2021-06-04 11:32:34 -0700

    added product page

--- react-ui/src/components/product/ProductPage.js
+++ react-ui/src/components/product/ProductPage.js
@@ -6,33 +6,32 @@ import { Card, Col, Row } from 'react-bootstrap'
 
 import { useLocation } from 'react-router-dom';
 const ProductPage = () => {
-    
+
     const location = useLocation()
-    const productId = location.pathname.charAt(location.pathname.length-1);
-   
-    const product = useSelector(state => state.reducer.products.find(product => (
-        product.id === productId
-    )));
+    const productId = location.pathname.charAt(location.pathname.length - 1);
+
+    const product = useSelector(state => state.reducer.products).find(product => product.id == productId)
+
+
+
     console.log(product)
-    return(
-        <div>
+    return (
 
-        </div>
-    //     <Card>
-    //     <Card.Body>
-    //         <Row>
-    //             <Col md={6}>
-    //                     <Card.Img variant="float" src={process.env.PUBLIC_URL + '/placeholder.png'} />
-    //             </Col>
-    //             <Col md={6}>
-    //                 <Card.Title className="text-center" >{props.product.title}</Card.Title>
+        <Card>
+            <Card.Body>
+                <Row>
+                    <Col md={6}>
+                        <Card.Img variant="float" src={process.env.PUBLIC_URL + '/placeholder.png'} />
+                    </Col>
+                    <Col md={6}>
+                        <Card.Title className="text-center" >{product.title}</Card.Title>
 
-    //                 <Card.Text>{props.product.description}</Card.Text>
-    //                 <Card.Subtitle>{props.product.price}</Card.Subtitle>
-    //             </Col>
-    //         </Row>
-    //     </Card.Body>
-    // </Card>
+                        <Card.Text>{product.description}</Card.Text>
+                        <Card.Subtitle>{product.price}</Card.Subtitle>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
     )
 }
 
